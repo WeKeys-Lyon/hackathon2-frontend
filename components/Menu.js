d26df7827bfc8f8211d8ad3570396bad424a5385
Brice WeKeys <formation@wekeys.fr>
2026-04-17 18:58:58 +0200

    Frontend - petites touches sur la homepage

--- components/Menu.js
+++ components/Menu.js
@@ -1,17 +1,26 @@
 import styles from '../styles/Menu.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-
+import Link from 'next/link';
+import Image from 'next/image';
 
 function Menu() {
+    const user = useSelector((state) => state.user.value);
 
+    const srcAvatar = '/avatars/goodenough.png';
+    const accountName = '@' + user.username;
     return (<>
-        <div>Logo</div>
+        <Link href="/home" className={styles.logo}>Logo</Link>
         <div>
             <div>
-                <div>Avatar Img</div>
+                <Image 
+                src={srcAvatar}
+                alt="Avatar Picture"
+                width={100}
+                height={100}
+                />
                 <div>
-                    <div>Firstname</div>
-                    <div>username</div>
+                    <div>{user.firstname}</div>
+                    <div>{accountName}</div>
                 </div>
             </div>
         </div>
